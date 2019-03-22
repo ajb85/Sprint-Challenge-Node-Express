ffc010f8496aa6826c96af7dedc1ba9ec5acd79e
ajb85 <ajbrush85@gmail.com>
2019-03-22 11:55:00 -0400

    Added get request for api/users

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const db = require("../data/helpers/projectModel.js");
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {});
+routes.get("/", async (req, res) => {
+  try {
+    const users = await db.get();
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Sorry, something went wrong fetching that list" });
+  }
+});
 
 routes.post("/", (req, res) => {});
 
